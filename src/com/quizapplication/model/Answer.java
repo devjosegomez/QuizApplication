d93ebf5269511d7c562ec771e91e4aad7437a82f
devjosegomez <devjosegomez@gmail.com>
2019-10-31 18:18:36 -0600

    Getters and setters of Answer class

--- src/com/quizapplication/model/Answer.java
+++ src/com/quizapplication/model/Answer.java
@@ -1,5 +1,17 @@
 package com.quizapplication.model;
 
 public class Answer {
-
+	String answer;
+	
+	public Answer(String answer) {
+		super();
+		this.answer = answer;
+	}
+	
+	//Getters and setters
+	public String getAnswer() {
+		return this.answer;
+	}
+	
+	
 }
