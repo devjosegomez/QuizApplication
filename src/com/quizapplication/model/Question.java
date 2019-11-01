ab9809d292224e3ba9ae6d9a073e8b8976f52894
devjosegomez <devjosegomez@gmail.com>
2019-10-31 18:18:50 -0600

    Getters and setters of Question class

--- src/com/quizapplication/model/Question.java
+++ src/com/quizapplication/model/Question.java
@@ -6,4 +6,40 @@ public class Question {
 	String option2;
 	String option3;
 	String option4;
+	Answer answer;
+	
+	public Question(String question, String option1, String option2, String option3, String option4, Answer answer) {
+		super();
+		this.question = question;
+		this.option1 = option1;
+		this.option2 = option2;
+		this.option3 = option3;
+		this.option4 = option4;
+	}
+	
+	//getters and setter
+	public String getQuestion() {
+		return this.question;
+	}
+	
+	public String getOption1() {
+		return this.option1;
+	}
+	
+	public String getOption2() {
+		return this.option2;
+	}
+	
+	public String getOption3() {
+		return this.option3;
+	}
+	
+	public String getOption4() {
+		return this.option4;
+	}
+	
+	public Answer getAnswer() {
+		return this.answer;
+	}
+	
 }
